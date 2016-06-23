b8e79402ba9418557f02502de405056739bd3a94
Spencer Zeke Depas <spencerdepas@gmail.com>
2016-06-23 13:11:44 -0400

    first commit to master

---
+++ src/com/clearfaun/mdlag/Main.java
@@ -0,0 +1,14 @@
+package com.clearfaun.mdlag;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        printHello();
+    }
+
+    private static void printHello(){
+        System.out.print("Hello");
+    }
+}
