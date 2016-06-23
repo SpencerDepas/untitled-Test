5fb7ab7a783a3e78b89d4bac9bde7291d96e83aa
Spencer Zeke Depas <spencerdepas@gmail.com>
2016-06-23 13:14:00 -0400

    made some big updates to dogfish class

---
+++ src/com/clearfaun/mdlag/DogFish.java
@@ -0,0 +1,15 @@
+package com.clearfaun.mdlag;
+
+/**
+ * Created by SpencerDepas on 6/23/16.
+ */
+public class DogFish {
+
+    int number = 44;
+
+
+    public int getNumber(){
+        return number;
+
+    }
+}
--- src/com/clearfaun/mdlag/Main.java
+++ src/com/clearfaun/mdlag/Main.java
@@ -6,6 +6,13 @@ public class Main {
 	// write your code here
 
         printHello();
+
+        DogFish dogFish = new DogFish();
+
+        dogFish.getNumber();
+
+        System.out.print(dogFish.getNumber());
+
     }
 
     private static void printHello(){
